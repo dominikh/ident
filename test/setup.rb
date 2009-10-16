9790930980210def505b74a9b73f519e299c2801
Dominik Honnef <dominikho@gmx.net>
2009-10-16 22:35:27 +0200

    making use of baretest/mocha

--- test/setup.rb
+++ test/setup.rb
@@ -1,15 +1,2 @@
 require File.absolute_path "#{__FILE__}/../../lib/ident"
-require 'mocha'
-
-BareTest.extend Mocha::API
-
-BareTest.toplevel_suite.teardown do
-  begin
-    BareTest.mocha_verify
-  rescue Mocha::ExpectationError => e
-    @failure_reason = e.message
-    @status = :failure
-  ensure
-    BareTest.mocha_teardown
-  end
-end
+require 'baretest/mocha'
